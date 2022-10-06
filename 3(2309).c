f01f1f6e8388b25b4e48a386b06b715f9fd725b4
이시윤 <nabee8005@sookmyung.ac.kr>
2022-10-06 18:43:08 +0900

    Lab03

--- 3(2309).c
+++ 3(2309).c
@@ -3,11 +3,13 @@
 
 int main() {
 	int height[9];
-	int num;
+	int sum=0,remain,fake1,fake2;
 
 	for (int i = 0; i < 9; i++) {
 		scanf("%d", &height[i]);
+		sum += height[i];
 	}
+	
 	for (int j = 0; j < 9; j++) {
 		for (int i = 0; i < 8; i++) {
 			int mid = 0;
@@ -19,8 +21,21 @@ int main() {
 			}
 		}
 	}
+	remain = sum - 100;
 
-		for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (remain == height[i] + height[j]) {
+				fake1 = i, fake2 = j;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (i == fake1 || i == fake2) {
+			continue;
+		}
 		printf("%d\n", height[i]);
 	}
 }

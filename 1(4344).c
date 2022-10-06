ab6002002ca75ab1d88241b5e21b98ca8ccb2d2f
siyunsiyun <109896924+siyunsiyun@users.noreply.github.com>
2022-10-06 19:56:10 +0900

    Add files via upload

---
+++ 1(4344).c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int test;
+
+	scanf("%d", &test);
+
+	for (int i = 0; i < test; i++) {
+		int person, grade[1000], count = 0;
+		int sum = 0;
+		double average, result;
+
+		scanf("%d", &person);
+
+		for (int j = 0; j < person; j++) {
+			scanf("%d", &grade[j]);
+			sum += grade[j];
+		}
+
+		average = (double)sum / (double)person;
+
+		for (int j = 0; j < person; j++) {
+			if (grade[j] > average) count++;
+		}
+
+		result = ((double)count / (double)person) * 100;
+
+		printf("%.3f%%\n", result);
+	}
+}
---
+++ 3(2309).c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int height[9];
+	int sum=0,remain,fake1,fake2;
+
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &height[i]);
+		sum += height[i];
+	}
+	
+	for (int j = 0; j < 9; j++) {
+		for (int i = 0; i < 8; i++) {
+			int mid = 0;
+			if (height[i] > height[i + 1]) {
+				mid = height[i];
+				height[i] = height[i + 1];
+				height[i + 1] = mid;
+
+			}
+		}
+	}
+	remain = sum - 100;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (remain == height[i] + height[j]) {
+				fake1 = i, fake2 = j;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (i == fake1 || i == fake2) {
+			continue;
+		}
+		printf("%d\n", height[i]);
+	}
+}
